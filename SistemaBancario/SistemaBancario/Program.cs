b703750a846b2f4b7e7047ab2acbfbe94a54d12a
fpaz19 <fpaz@pucp.pe>
2017-09-29 17:06:09 -0500

    Lineas

--- SistemaBancario/SistemaBancario/Program.cs
+++ SistemaBancario/SistemaBancario/Program.cs
@@ -17,6 +17,10 @@ namespace SistemaBancario
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+
         }
     }
 }
